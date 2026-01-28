7a141fbdc2e8bc780941b23baf04448dc7082c54
ranger097 <ranger097@proton.me>
2026-01-28 17:28:00 -0500

    updated configs

--- waybar_toggle.py
+++ waybar_toggle.py
@@ -3,13 +3,24 @@ toggleFile = "/home/ranger/Github/Scripts/toggle.txt"
 
 with open(toggleFile,"r") as file:
     x = file.readline()
-    if x == "True":
+    if x == "1":
+        subprocess.run(["pkill","waybar"])
         subprocess.Popen(["waybar", "-c", "/home/ranger/.config/waybar/top.jsonc", "-s", "/home/ranger/.config/waybar/top.css"])
         subprocess.Popen(["waybar", "-c", "/home/ranger/.config/waybar/bottom.jsonc", "-s", "/home/ranger/.config/waybar/bottom.css" ])
         with open(toggleFile,"w") as file:
-            file.write("False")
+            file.write("2")
+    elif x == "2":
+        subprocess.run(["pkill","waybar"])
+        subprocess.Popen(["waybar", "-c", "/home/ranger/.config/waybar/left.jsonc", "-s", "/home/ranger/.config/waybar/left.css"])
+        with open(toggleFile,"w") as file:
+            file.write("3")
+    elif x == "3":
+        subprocess.run(["pkill","waybar"])
+        subprocess.Popen(["waybar", "-c", "/home/ranger/.config/waybar/right.jsonc", "-s", "/home/ranger/.config/waybar/right.css"])
+        with open(toggleFile,"w") as file:
+            file.write("4")
     else:
         subprocess.run(["pkill","waybar"])
         with open(toggleFile,"w") as file:
-            file.write("True")
+            file.write("1")
 
