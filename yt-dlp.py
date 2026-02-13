0df9160daae4f7058370ccd87090362f5fc1d587
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-02-12 20:15:54 -0500

    updated configs

--- yt-dlp.py
+++ yt-dlp.py
@@ -1,2 +1,4 @@
-#will be used with subprocess and curses to download playlist fast and efficiently.
+#will be used with subprocess and npyscreen to download playlist fast and efficiently.
 #yt-dlp -x --audio-format mp3 --audio-quality 0 --cookies-from-browser firefox --embed-metadata --embed-thumbnail --add-metadata "https://music.youtube.com/playlist?list=PLJ77UmXjk2yHfiI8a_aN5tjf9ZyZP_HxB"
+import npyscreen
+
